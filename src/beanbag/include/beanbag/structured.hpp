6e53554d4f021a443c345df64c25952f2e7e1a17
Kirit Sælensminde <k@kirit.com>
2012-03-04 10:16:04 +0700

    Empty file to start work on the structured object view.

---
+++ src/beanbag/include/beanbag/structured.hpp
@@ -0,0 +1,23 @@
+/*
+    Copyright 2012 Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#pragma once
+#ifndef BEANBAG_STRUCTURED_HPP
+#define BEANBAG_STRUCTURED_HPP
+
+
+#include <beanbag/raw.hpp>
+
+
+namespace beanbag {
+
+
+}
+
+
+#endif // BEANBAG_STRUCTURED_HPP
