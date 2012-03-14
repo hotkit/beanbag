bafef07fb2c8133b5b6d298580dba288aef06b37
Kirit Sælensminde <k@kirit.com>
2012-03-14 14:15:54 +0700

    The controller now processes the 410 response as a success for delete.

--- usr/share/beanbag/wiki/media/wiki.js
+++ usr/share/beanbag/wiki/media/wiki.js
@@ -38,13 +38,13 @@ function WikiController($location, $http) {
     this.deletepage = function() {
         $http.delete($location.path(),
                 {headers:{'If-Match': self.meta.version}}).
-            success(function(data, status, headers, config) {
-                self.page = {};
-                self.tab = 'show';
-                self.meta.version = null;
-            }).
             error(function(data, status, headers, config) {
-                console.log("error", data, status, headers, config);
+                if ( status == 410 ) {
+                    self.page = {};
+                    self.tab = 'edit';
+                    self.meta.version = null;
+                } else
+                    console.log("error", data, status, headers, config);
             });
     }
     self.tab = 'edit';
