3234bd3b7f2093fa527c8dbf7530197e88a3b6fe
Kirit Sælensminde <k@kirit.com>
2012-05-08 08:44:49 +0700

    Refactored out the common code.

--- usr/share/beanbag/gtf/media/gtf-spec.js
+++ usr/share/beanbag/gtf/media/gtf-spec.js
@@ -1,12 +1,21 @@
 
 describe("GTF", function() {
+    var scope, http, ctrl;
+    beforeEach(function() {
+        scope = {};
+        http = {};
+        ctrl = new controller(scope, http);
+    });
     it("initialises", function() {
-        var scope = {}, http = {};
-        var ctrl = new controller(scope, http);
         expect(scope.letters).toEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         expect(scope.letter1).toEqual("G");
         expect(scope.letter2).toEqual("T");
         expect(scope.letter3).toEqual("F");
+    });
+    it("choose changes the first letter", function() {
+        expect(scope.letter1).toEqual("G");
+        scope.choose(1, 'A');
+        expect(scope.letter1).toEqual("A");
     });
 });
 
