e57d2899ed2144cc9f8bbec81149e7d0aabc68f5
Kirit Sælensminde <k@kirit.com>
2012-05-08 10:40:05 +0700

    We now get a mock HTTP response.

--- usr/share/beanbag/gtf/media/gtf-spec.js
+++ usr/share/beanbag/gtf/media/gtf-spec.js
@@ -1,21 +1,27 @@
 
 describe("GTF", function() {
-    var scope, http, ctrl;
-    beforeEach(function() {
-        scope = {};
-        http = {};
-        ctrl = new controller(scope, http);
-    });
+    var $scope, ctrl, $httpBackend;
+    beforeEach(angular.mock.inject(function($injector, $controller, $rootScope) {
+        $httpBackend = $injector.get('$httpBackend');
+        $scope = $rootScope.$new();
+        ctrl = $controller(controller, {$scope: $scope});
+    }));
     it("initialises", function() {
-        expect(scope.letters).toEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        expect(scope.letter1).toEqual("G");
-        expect(scope.letter2).toEqual("T");
-        expect(scope.letter3).toEqual("F");
+        expect($scope.letters).toEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        expect($scope.letter1).toEqual("G");
+        expect($scope.letter2).toEqual("T");
+        expect($scope.letter3).toEqual("F");
     });
     it("choose changes the first letter", function() {
-        expect(scope.letter1).toEqual("G");
-        scope.choose(1, 'A');
-        expect(scope.letter1).toEqual("A");
+        expect($scope.letter1).toEqual("G");
+        $scope.choose(1, 'A');
+        expect($scope.letter1).toEqual("A");
+    });
+    it("tla returns the right result", function() {
+        $httpBackend.when("GET", "/GTF").respond([true]);
+        expect($scope.tla()).toEqual("GTF");
+        $httpBackend.flush();
+        expect($scope.definitions).toEqual([true]);
     });
 });
 
